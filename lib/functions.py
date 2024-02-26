47ad543ef07f988e29b621e5b9d1440dacd4dad5
Omoding-web <samuel.omoding@student.moringaschool.com>
2024-02-26 17:03:50 +0300

    Assignment donne

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,35 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
+    print("Hello, programmer!")
     pass
 
 def greet(name):
-    pass
+    print(f"Hello, {name}!")
+
+greet("Naureen")
+pass
 
 def greet_with_default(name="programmer"):
-    pass
+  print(f"Hello, {name}!")
+
+greet_with_default()
+greet_with_default("Sunny")
+pass
 
 def add(num1, num2):
-    pass
+    return num1 + num2
+
+sum_result = add(45, 55)
+print(sum_result)
+pass
 
 def halve(number):
-    pass
+    if not isinstance(number, (int, float)):
+        return None
+    
+    return number / 2
+
+print(halve(100))
+print(halve(99.0))
+pass
